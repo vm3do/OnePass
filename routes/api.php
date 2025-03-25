880f36f1744bad05c606ebdaa3041061a337a609
F99 <farajiomar99@gmail.com>
2025-03-25 14:11:44 +0000

    Add password import route with authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,5 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('/passwords/import', [PasswordController::class, 'importPasswords'])->middleware('auth:sanctum');
