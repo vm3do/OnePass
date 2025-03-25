1577c76a4bdba62c775e5f78611b8cac31ac429b
meriem el mecaniqui <melecaniqui@gmail.com>
2025-03-25 16:12:49 +0100

    rate limiting

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,11 @@ use App\Models\Ip;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Mail\LoginAttemptWarningMail;
+use Illuminate\Support\Facades\Cache;
 
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Mail;
 class AuthController extends Controller
 {
     public function register(Request $request){
@@ -38,14 +42,29 @@ class AuthController extends Controller
             'email' => 'required|exists:users',
             'password' => 'min:6|required',
         ]);
+        $key='user_email:'.$request->email;
+       if (RateLimiter::tooManyAttempts($key,3)) {
+        if (!Cache::has('alert_sent_by'.$request->email)) {
+       Mail::to($request->email)->send( new  LoginAttemptWarningMail());
+       Cache::put('alert_sent_by' . $request->email, true, 3600);
+        }
+
+   
+
+
+   return response()->json(['message' => 'Trop de tentatives, veuillez réessayer plus tard.'], 429);
+}
 
         $user = User::where('email', $request->email)->first();
         if(!$user || !Hash::check($request->password, $user->password)){
+            RateLimiter::increment($key);
             return response()->json([
                 'error' => 'incorrect credentials'
             ], 401);
         };
+        RateLimiter::hit($key,180);
 
+        RateLimiter::clear($key);
         $token = $user->createToken($user->email);
 
         return response()->json([
---
+++ resources/views/mail.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+        <div class="header">
+            <h1>Alerte de Connexion Suspecte</h1>
+        </div>
+        <div class="message">
+            <p>Bonjour,</p>
+            <p>Nous avons détecté plusieurs tentatives de connexion échouées sur votre compte. Si vous ne reconnaissez pas cette activité, il est recommandé de prendre des mesures pour sécuriser votre compte.</p>
+            <p>Si vous avez besoin d'aide ou si vous pensez que votre compte a été compromis, veuillez cliquer sur le bouton ci-dessous pour sécuriser votre compte :</p>
+        
+        </div>
+       
+    </div>
