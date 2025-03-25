073f4c078eb575cdba133d9f6beb22182f211dba
meriem el mecaniqui <melecaniqui@gmail.com>
2025-03-25 16:23:08 +0100

    rate limiting

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,9 +12,11 @@ use Illuminate\Support\Facades\Validator;
 
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Mail;
+
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
 
         $validated = Validator::make($request->all(), [
             'name' => 'required|min:3',
@@ -22,7 +24,7 @@ class AuthController extends Controller
             'password' => 'required|min:6|confirmed'
         ]);
 
-        if($validated->fails()){
+        if ($validated->fails()) {
             return response()->json([
                 'error registering' => $validated->errors()
             ], 401);
@@ -40,42 +42,42 @@ class AuthController extends Controller
             'user_ip' => $user_ip,
             'token' => $token->plainTextToken,
         ], 201);
-
     }
-    
-    public function login(Request $request){
+
+    public function login(Request $request)
+    {
 
         $validated = Validator::make($request->all(), [
             'email' => 'required|exists:users',
             'password' => 'min:6|required',
         ]);
 
-        if($validated->fails()){
+        if ($validated->fails()) {
             return response()->json([
                 'error login in' => $validated->errors()
             ], 401);
         }
-        $key='user_email:'.$request->email;
-       if (RateLimiter::tooManyAttempts($key,3)) {
-        if (!Cache::has('alert_sent_by'.$request->email)) {
-       Mail::to($request->email)->send( new  LoginAttemptWarningMail());
-       Cache::put('alert_sent_by' . $request->email, true, 3600);
+        $key = 'user_email:' . $request->email;
+        if (RateLimiter::tooManyAttempts($key, 3)) {
+            if (!Cache::has('alert_sent_by' . $request->email)) {
+                Mail::to($request->email)->send(new  LoginAttemptWarningMail());
+                Cache::put('alert_sent_by' . $request->email, true, 3600);
+            }
+
+
+
+
+            return response()->json(['message' => 'Trop de tentatives, veuillez réessayer plus tard.'], 429);
         }
 
-   
-
-
-   return response()->json(['message' => 'Trop de tentatives, veuillez réessayer plus tard.'], 429);
-}
-
         $user = User::where('email', $request->email)->first();
-        if(!$user || !Hash::check($request->password, $user->password)){
+        if (!$user || !Hash::check($request->password, $user->password)) {
             RateLimiter::increment($key);
             return response()->json([
                 'error' => 'incorrect credentials'
             ], 401);
         };
-        RateLimiter::hit($key,180);
+        RateLimiter::hit($key, 180);
 
         RateLimiter::clear($key);
         $token = $user->createToken($user->email);
@@ -84,15 +86,14 @@ class AuthController extends Controller
             'user' => $user,
             'token' => $token->plainTextToken,
         ], 201);
-
     }
-    
-    public function logout(Request $request){
+
+    public function logout(Request $request)
+    {
         $request->user()->tokens()->delete();
 
         return response()->json([
             'message' => 'you are logged out'
         ], 200);
     }
-    
 }
