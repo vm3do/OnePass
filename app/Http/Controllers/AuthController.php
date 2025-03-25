4e6a045b0a7269fcc55081ec768880bbe8e336ff
vm3do <medoavvadi@gmail.com>
2025-03-25 11:17:19 +0000

    created IP contoller, model and migrate and added a role to users table

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+
+        $validated = $request->validate([
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        User::create($validated);
+        
+
+    }
+    
+    public function login(){
+
+    }
+    
+    public function logout(){
+
+    }
+    
 }
